5333c89d686ca9a681376eaee6ae2db5eb7c4464
Nelu Garila <nelugarila@MacBook-Air-Nelu.local>
2026-02-23 16:06:13 +0200

    Ready for deployment

--- src/App.js
+++ src/App.js
@@ -207,7 +207,6 @@ function Navbar() {
       {menuOpen && (
         <div className="mobile-drawer">
           <div className="mobile-drawer-logo">Cod<span>][</span>ng</div>
-          <button className="mobile-drawer-close" onClick={close}>✕</button>
           <a href="#about"     className="mobile-link" onClick={close}>About</a>
           <a href="#portfolio" className="mobile-link" onClick={close}>Portfolio</a>
           <a href="#services"  className="mobile-link" onClick={close}>Services</a>
